e9b5e0682fe0a3f4f4fdf51680182106a28212e3
Kishore-Karic <kirukishore89@gmail.com>
2023-02-15 20:04:00 +0530

    Horizontal Movement
    Added horizontal movement to character from script.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
 
+    public float speed;
+
+    float horizontal;
+    float verticle;
+    bool crouch;
+
     void Awake()
     {
         Debug.Log("Player Controller Awake");
@@ -13,30 +19,49 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        horizontal = Input.GetAxisRaw("Horizontal");
+        verticle = Input.GetAxisRaw("Vertical");
+        crouch = Input.GetKey(KeyCode.LeftControl);
+
+        MoveCharacter(horizontal);
+        PlayerMovementAnimation(horizontal, verticle, crouch);
+    }
+
+    void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void PlayerMovementAnimation(float horizontal, float verticle, bool crouch)
+    {
+        // Horizontal move animation
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
 
-        if(speed > 0)
+        if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
-        } else if(speed < 0)
+        }
+        else if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
 
-        float jump = Input.GetAxisRaw("Vertical");
-        if(jump > 0)
+        // jump animation
+        if (verticle > 0)
         {
             animator.SetBool("Jump", true);
-        } else
+        }
+        else
         {
             animator.SetBool("Jump", false);
         }
 
-        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        // crouch animation
         animator.SetBool("Crouch", crouch);
     }
 }
