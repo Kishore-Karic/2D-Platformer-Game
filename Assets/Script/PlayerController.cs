52e63bede65903d0c9522cd756beff0f840c7d19
Kishore-Karic <kirukishore89@gmail.com>
2023-02-15 17:42:36 +0530

    Scripting Animation & Flipping
    Play animation according to Horizontal input and Flipping character.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+
     void Awake()
     {
         Debug.Log("Player Controller Awake");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Update()
     {
-        Debug.Log("Collide with " + collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+
+        if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        } else if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
