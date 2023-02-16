62c8a0d60a8e0adac8f35157c4d0dbf7d20bb959
Kishore-Karic <kirukishore89@gmail.com>
2023-02-16 17:20:22 +0530

    Death After Falling From Platform
    Added fall animation when fall from platform.
    Added Death animation to play death when fall from platform.

---
+++ Assets/Script/GroundCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "FallDetector")
+        {
+            playerController.SetGround(true);
+            playerController.animator.SetBool("Death", true);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Platform")
+        {
+            playerController.SetJump(false);  //isJumping = false;
+            playerController.SetGround(true);  //isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Platform")
+        {
+            playerController.SetGround(false);  //isGrounded = false;
+        }
+    }
+}
